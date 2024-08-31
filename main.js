c4685136aa5a6090fb0e0f9ab42b89fb4a6fb898
Ben Foxall <benfoxall@gmail.com>
2024-08-31 15:14:01 +0000

    prompt stream

--- main.js
+++ main.js
@@ -2,31 +2,72 @@ const input = document.querySelector('input')
 const output = document.querySelector('output')
 const errorOutput = document.querySelector('output.err')
 
+
+
 if (!('ai' in window)) {
-    errorOutput.innerText = 'Error: AI API is not supported by this browser. Please use Chrome Canary.'
+    errorOutput.innerText = 'Error: window.ai not found (needs Chrome Canary)'
 } else {
     try {
         const session = await ai.assistant.create({
             monitor(m) {
                 m.addEventListener("downloadprogress", e => {
-                    console.log(`Downloaded ${e.loaded} of ${e.total} bytes.`);
+                    errorOutput.innerText = `Downloaded ${e.loaded} of ${e.total} bytes.`
                 });
             }
-
         });
+
         console.log(session)
 
-        session.prompt('yes, or no?')
+        let pending = false;
+
+        async function requestLoop() {
+            if (!pending) return setTimeout(requestLoop, 100)
+
+            input.classList.add('requesting')
+            try {
+                const stream = await session.promptStreaming(pending)
+                for await (const chunk of stream) {
+                    console.log(chunk);
+                    output.innerText = chunk;
+                }
+
+
+            } catch (e) {
+                errorOutput.innerText = `err: ${e.message}`
+            }
+
+            input.classList.remove('requesting')
+
+            pending = false
+
+            return setTimeout(requestLoop, 1000)
+        }
+
+        requestLoop();
+
+
+        let timer;
+        function debounce() {
+            clearTimeout(timer);
+
+            input.classList.add('debounced')
+            timer = setTimeout(() => {
+                input.classList.remove('debounced');
+                pending = input.value;
+
+            }, 500);
+        }
+
+        // rather than debounce, a prioritised queue?
+
 
         input.addEventListener('input', e => {
-            console.log(input.value)
-            if (input.value.endsWith('?')) {
-                session.prompt(input.value)
-                    .then(value => output.innerText = value)
-            }
+            debounce()
         })
 
     } catch (e) {
         errorOutput.innerText = `Error: ${e.message}`
     }
 }
+
+// assessing a PR Description
